b19721550d9adfdec3cb85853b191c5d94617fc3
martynasraila <railamartynas@gmail.com>
2022-02-25 16:54:21 +0200

    Fixed logout

--- src/services/logout.ts
+++ src/services/logout.ts
@@ -1,5 +1,25 @@
-const logout = () => {
+import { API } from "~/constants";
+import getUrl from "~/utils/getUrl";
 
+const logout = async () => {
+	const key = "token";
+	const token = localStorage.getItem(key);
+	const url = getUrl(API.Logout);
+	localStorage.getItem(key) && localStorage.removeItem(key);
+	const response = await fetch(url, {headers: {'Authorization': `token ${token}`}});
 };
 
 export default logout;
+
+// const login = async (username: string, password: string) => {
+//     const url = getUrl(API.Login, {
+//       username,
+//       password,
+//     });
+
+//     const response = await fetch(url);
+//     const data = await response.json();
+//     const { token } = data;
+
+//     localStorage.setItem('token', token);
+//   };
