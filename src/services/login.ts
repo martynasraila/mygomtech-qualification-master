284e7bb0b164dede2e530d1230816460f94eb154
martynasraila <railamartynas@gmail.com>
2022-03-02 21:26:13 +0200

    Updated authentification to use post, returning and printing login status

--- src/services/login.ts
+++ src/services/login.ts
@@ -1,20 +1,32 @@
-import {API} from '~/constants';
-import getUrl from '../utils/getUrl';
+import { API } from "~/constants";
+import getUrl from "../utils/getUrl";
 
 const login = async (username: string, password: string) => {
-  const url = getUrl(API.Login, {
-    username,
-    password,
-  });
+	const url = getUrl(API.Login);
 
-  const response = await fetch(url);
-  if(!response.ok) {
-    return;
-  }
-  const data = await response.json();
-  const { token } = data;
+	const response = await fetch(url, {
+		method: "POST",
+		body: JSON.stringify({ username, password }),
+		headers: {
+			"Content-Type": "application/json",
+		},
+	});
 
-  localStorage.setItem('token', token);
+	if (!response.ok) {
+		if (response.status === 404) {
+			return "Username not found";
+		} else if (response.status === 401) {
+			return "Password is incorrect";
+		}
+	}
+
+	const data = await response.json();
+	const { token } = data;
+
+	if (token) {
+		localStorage.setItem("token", token);
+	}
+	return;
 };
 
 export default login;
