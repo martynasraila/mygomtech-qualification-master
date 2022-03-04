ba2660a3bc0719387c71a517e942972a25ce1878
martynasraila <railamartynas@gmail.com>
2022-03-04 14:36:05 +0200

    Removed unecessary comments

--- src/services/logout.ts
+++ src/services/logout.ts
@@ -6,20 +6,7 @@ const logout = async () => {
 	const token = localStorage.getItem(key);
 	const url = getUrl(API.Logout);
 	token && localStorage.removeItem(key);
-	const response = await fetch(url, {headers: {'Authorization': `Bearer ${token}`}});
+	await fetch(url, {headers: {'Authorization': `Bearer ${token}`}});
 };
 
 export default logout;
-
-// const login = async (username: string, password: string) => {
-//     const url = getUrl(API.Login, {
-//       username,
-//       password,
-//     });
-
-//     const response = await fetch(url);
-//     const data = await response.json();
-//     const { token } = data;
-
-//     localStorage.setItem('token', token);
-//   };
